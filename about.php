bc2d0e8a3c6f34052a2e1e30d68cab9873067b2d
ahmadjajja794-create <ahmadjajja794@gmail.com>
2026-06-09 22:52:25 +0500

    complete about page

---
+++ about.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About - Library Management System</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<nav>
+    <div class="logo">📖 Library Management System</div>
+
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="books.php">Books</a></li>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li><a href="profile.php">Profile</a></li>
+        <li><a href="admin_login.php">Admin</a></li>
+        <li><a class="active" href="about.php">About</a></li>
+    </ul>
+</nav>
+
+<section class="page-header">
+    <h1>About  University Library</h1>
+    <p>Digital platform for reading and exploring books online.</p>
+</section>
+
+<section class="books">
+
+    <div class="card">
+
+        <h3>📚 About Our Library</h3>
+
+        <p>
+            UMT University Library is a modern digital platform
+            where students can read, explore, and download books online.
+            Our mission is to make learning simple, fast, and accessible.
+        </p>
+
+        <br>
+
+        <p>
+            This Library Management System helps students manage books,
+            explore categories, and improve their learning experience.
+        </p>
+
+        <br><br>
+
+        <h3>👨‍💻 Developer</h3>
+
+        <p>Ahmad Raza</p>
+
+        <br><br>
+
+        <h3>🤝 Supporters & Contributors</h3>
+
+        <p>Muhammad Anas</p>
+        <p>Muhammad Umair Hassan</p>
+        <p>Amjad Ali Awan</p>
+        <p>Muhammad Dawood</p>
+        <p>Munawar Hussain</p>
+        <p>Ehtasham Bilal</p>
+
+    </div>
+
+</section>
+
+<footer>
+    <p>© 2026  University Library. All Rights Reserved.</p>
+    <p>Developed by Ahmad Raza</p>
+</footer>
+
+</body>
+</html>
