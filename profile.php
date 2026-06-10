c97e15bcc9469ae862f8133be3e902742f3b8ad4
ahmadjajja794-create <ahmadjajja794@gmail.com>
2026-06-10 18:38:15 +0500

    fix profile page error

--- profile.php
+++ profile.php
@@ -9,53 +9,34 @@ if(!isset($_SESSION['user_id'])){
 
 $user_id = $_SESSION['user_id'];
 
-$result = mysqli_query($conn,
-"SELECT * FROM users WHERE id='$user_id'");
+$result = mysqli_query($conn, "SELECT * FROM users WHERE id='$user_id'");
 
 $user = mysqli_fetch_assoc($result);
 ?>
 
 <!DOCTYPE html>
 <html>
-
 <head>
-
     <title>My Profile</title>
-
-    <link rel="stylesheet" href="style.css">
-
+    <link rel="stylesheet" href="style.css?v=1000">
 </head>
 
 <body>
 
 <nav>
-
-    <div class="logo">
-        📖 Library Management System
-    </div>
+    <div class="logo">📖 Library Management System</div>
 
     <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="books.php">Books</a></li>
+        <li><a class="active" href="profile.php">Profile</a></li>
 
-        <li>
-            <a href="index.php">Home</a>
-        </li>
+        <?php if(isset($_SESSION['email']) && $_SESSION['email'] == "universitylibrary172@gmail.com"){ ?>
+            <li><a href="admin_dashboard.php">Admin Panel</a></li>
+        <?php } ?>
 
-        <li>
-            <a href="books.php">Books</a>
-        </li>
-
-        
-
-        <li>
-            <a class="active" href="profile.php">Profile</a>
-        </li>
-
-        <li>
-            <a href="logout.php">Logout</a>
-        </li>
-
+        <li><a href="logout.php">Logout</a></li>
     </ul>
-
 </nav>
 
 <section class="form-section">
@@ -64,39 +45,22 @@ $user = mysqli_fetch_assoc($result);
 
         <h2>My Profile</h2>
 
-        <p>
-            <b>Name:</b>
-            <?php echo $user['fullname']; ?>
-        </p>
+        <p><b>Name:</b> <?php echo htmlspecialchars($user['fullname']); ?></p>
 
-        <p>
-            <b>Email:</b>
-            <?php echo $user['email']; ?>
-        </p>
+        <p><b>Email:</b> <?php echo htmlspecialchars($user['email']); ?></p>
 
-        <p>
-            <b>Password:</b>
-            ********
-        </p>
+        <p><b>Password:</b> ********</p>
 
         <br>
 
         <a href="change_password.php">
-
-            <button>
-                Change Password
-            </button>
-
+            <button>Change Password</button>
         </a>
 
         <br><br>
 
         <a href="logout.php">
-
-            <button>
-                Logout
-            </button>
-
+            <button>Logout</button>
         </a>
 
     </div>
@@ -105,4 +69,3 @@ $user = mysqli_fetch_assoc($result);
 
 </body>
 </html>
-
